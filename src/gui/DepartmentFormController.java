9e07afd8c1e73fa2bc93bde0dc6df351274599c7
viniciusdiasdev <viniciusdiasdev@gmail.com>
2021-12-29 17:02:18 -0300

    Passando o departamento obj para o form

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,9 +9,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
-
+	
+	private Department entity;
+	
 	@FXML
 	private TextField txtId;
 	
@@ -35,6 +38,18 @@ public class DepartmentFormController implements Initializable {
 	@FXML
 	public void onBtCancelAction() {
 		System.out.println("Foi 2");
+	}
+	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+	
+	public void updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
 	}
 	
 	@Override
